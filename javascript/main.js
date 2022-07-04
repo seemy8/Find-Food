5cc24b47596c1ccba5176e0051e984e0af0994c0
seemy8 <46694347+seemy8@users.noreply.github.com>
2022-07-04 16:57:03 +1200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+let btn = document.getElementById('act-btn');
+let overlay= document.getElementById('overlay');
+
+btn.addEventListener('click', function() {
+
+  overlay.style.display='grid'; 
+  
+
+});
+
+});
